839dbe8a3f94f59a256d9aea91ede8fc4b549cef
Smirnov Oleksandr <ss2316544@gmail.com>
2024-01-13 01:50:49 +0200

    refactor(http): utils for responding w/ errs

--- internal/adapters/primary/web/response.go
+++ internal/adapters/primary/web/response.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"log/slog"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +11,22 @@ type response struct {
 	Message string `json:"message"`
 }
 
-func newRespones(c *gin.Context, status int, msg string) {
+func newError(c *gin.Context, status int, msg string) {
 	slog.With("status", status).Error(msg)
 	c.AbortWithStatusJSON(status, response{msg})
+}
+
+func newInternalError(c *gin.Context, err error, msg ...string) {
+	slog.With("status", "internal error").Error(err.Error())
+
+	if len(msg) == 1 {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, response{
+			Message: msg[0],
+		})
+		return
+	}
+
+	c.AbortWithStatusJSON(http.StatusInternalServerError, response{
+		Message: "internal error",
+	})
 }
