3cefa497e25dc97a1321901183917c21ab1dd009
Smirnov Oleksandr <ss2316544@gmail.com>
2024-01-13 01:24:05 +0200

    refactor: add some of logger

--- internal/core/services/notesrv/notesrv.go
+++ internal/core/services/notesrv/notesrv.go
@@ -2,6 +2,7 @@ package notesrv
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/google/uuid"
 	"github.com/olexsmir/onasty/internal/core/domain"
@@ -23,6 +24,8 @@ func New(store ports.NoteStorer) *Service {
 func (s *Service) Create(ctx context.Context, inp domain.Note) (string, error) {
 	// TODO: validate input
 
+	slog.With("inp", inp).Info("createing note")
+
 	if inp.Slug == "" {
 		inp.Slug = uuid.New().String()
 	}
@@ -31,5 +34,7 @@ func (s *Service) Create(ctx context.Context, inp domain.Note) (string, error) {
 }
 
 func (s *Service) GetBySlug(ctx context.Context, inp string) (domain.Note, error) {
+	slog.With("slug", inp).Info("getting note by slug")
+
 	return s.store.GetBySlug(ctx, inp)
 }
