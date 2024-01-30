cd67cafcc064e46249235f8c415c6a9508a88e97
Smirnov Oleksandr <ss2316544@gmail.com>
2024-01-30 23:06:33 +0200

    add one more test

--- e2e/notes_test.go
+++ e2e/notes_test.go
@@ -2,12 +2,38 @@ package e2e
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/olexsmir/onasty/internal/core/domain"
 )
 
 type noteResponse struct {
 	Slug string `json:"slug"`
+}
+
+func (s *AppTestSuite) TestNote_Create_AllOpts() {
+	content := "testing"
+	slug := "some-semi-random-slug"
+	burnBeforeExpiration := true
+	expireAt := time.Now().Add(7 * time.Minute)
+
+	httpResp := s.httpRequest(http.MethodPost, "/api/v1/note", s.jsonify(map[string]any{
+		"content":                content,
+		"slug":                   slug,
+		"burn_before_expiration": burnBeforeExpiration,
+		"expires_at":             expireAt,
+	}))
+
+	var res noteResponse
+	s.readBodyAndUnjsonify(httpResp.Body, &res)
+
+	dbNote := s.getNoteFromDBBySlug(res.Slug)
+
+	s.Equal(http.StatusCreated, httpResp.Code)
+	s.Equal(slug, dbNote.Slug)
+	s.Equal(content, dbNote.Content)
+	s.Equal(burnBeforeExpiration, dbNote.BurnBeforeExpiration)
+	s.Equal(expireAt.Unix(), dbNote.ExpiresAt.Unix())
 }
 
 func (s *AppTestSuite) TestNote_Create_CantBeWirhoutContent() {
@@ -25,10 +51,10 @@ func (s *AppTestSuite) TestNote_Create_RandomSlug() {
 		"content": "testing",
 	}))
 
-	var note noteResponse
-	s.readBodyAndUnjsonify(httpResp.Body, &note)
+	var res noteResponse
+	s.readBodyAndUnjsonify(httpResp.Body, &res)
 
-	dbNote := s.getNoteFromDBBySlug(note.Slug)
+	dbNote := s.getNoteFromDBBySlug(res.Slug)
 
 	s.Equal(http.StatusCreated, httpResp.Code)
 	s.NotEmpty(dbNote)
@@ -43,12 +69,12 @@ func (s *AppTestSuite) TestNote_Create_ExplicitSlug() {
 		"slug":    slug,
 	}))
 
-	var note noteResponse
-	s.readBodyAndUnjsonify(httpResp.Body, &note)
+	var res noteResponse
+	s.readBodyAndUnjsonify(httpResp.Body, &res)
 
-	dbNote := s.getNoteFromDBBySlug(note.Slug)
+	dbNote := s.getNoteFromDBBySlug(res.Slug)
 
 	s.Equal(http.StatusCreated, httpResp.Code)
-	s.Equal(note.Slug, dbNote.Slug)
+	s.Equal(res.Slug, dbNote.Slug)
 	s.Equal(content, dbNote.Content)
 }
