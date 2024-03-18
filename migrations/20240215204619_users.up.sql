bbec6d7614470eaad13871627875cfbdbb165fae
Smirnov Oleksandr <ss2316544@gmail.com>
2024-03-18 22:28:04 +0200

    some db fixes

--- migrations/20240215204619_users.up.sql
+++ migrations/20240215204619_users.up.sql
@@ -1,6 +1,6 @@
 create table users (
   id uuid primary key default uuid_generate_v4(),
-  username varchar(255) not null,
+  username varchar(255) not null unique,
   email varchar(255) not null unique,
   password varchar(255) not null,
   created_at timestamptz not null default now(),
