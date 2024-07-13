e9316db8ccc5fe9f499d47b89fe5b6463e7eb608
Smirnov Oleksandr <ss2316544@gmail.com>
2024-07-13 21:54:35 +0300

    test(e2e): test signin

--- e2e/apiv1_auth_test.go
+++ e2e/apiv1_auth_test.go
@@ -9,8 +9,8 @@ type apiv1AuthSignUpRequest struct {
 }
 
 func (e *AppTestSuite) TestAuthV1_SignUP() {
-	username := "test"
-	email := "test@test.com"
+	username := "test" + e.uuid()
+	email := e.uuid() + "test@test.com"
 	password := "password"
 
 	httpResp := e.httpRequest(
@@ -65,4 +65,41 @@ func (e *AppTestSuite) TestAuthV1_SignUP_badrequest() {
 
 		e.Equal(http.StatusBadRequest, httpResp.Code)
 	}
+}
+
+type apiv1AuthSignInRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type apiv1AuthSignInResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+func (e *AppTestSuite) TestAuthV1_SignIn() {
+	email := e.uuid() + "email@email.com"
+	password := "qwerty"
+
+	uid := e.insertUserIntoDB("test", email, password)
+
+	httpResp := e.httpRequest(
+		http.MethodPost,
+		"/api/v1/auth/signin",
+		e.jsonify(apiv1AuthSignInRequest{
+			Email:    email,
+			Password: password,
+		}),
+	)
+
+	var body apiv1AuthSignInResponse
+	e.readBodyAndUnjsonify(httpResp.Body, &body)
+
+	session := e.getLastUserSessionByUserId(uid)
+	parsedToken, err := e.jwtTokenizer.Parse(body.AccessToken)
+	e.require.NoError(err)
+
+	e.Equal(http.StatusOK, httpResp.Code)
+	e.Equal(body.RefreshToken, session.RefreshToken)
+	e.Equal(parsedToken.UserID, uid.String())
 }
