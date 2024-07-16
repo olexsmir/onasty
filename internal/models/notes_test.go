4f575ab88bd2f7be1cd8bfe6f4bf8a90a9af744c
Smirnov Oleksandr <ss2316544@gmail.com>
2024-07-17 02:40:13 +0300

    test(models): test validation of note

--- internal/models/notes_test.go
+++ internal/models/notes_test.go
@@ -4,8 +4,44 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
+	assert "github.com/stretchr/testify/require"
 )
+
+func TestNote_Validate(t *testing.T) {
+	tests := []struct {
+		name      string
+		note      Note
+		willError bool
+		error     error
+	}{
+		// NOTE: there no need to test if note is expired since it tested in IsExpired test
+		{
+			name: "ok",
+			note: Note{
+				Content:   "some wired ass content",
+				ExpiresAt: time.Now().Add(time.Hour),
+			},
+			willError: false,
+		},
+		{
+			name:      "content missing",
+			note:      Note{Content: ""},
+			willError: true,
+			error:     ErrNoteContentIsEmpty,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.note.Validate()
+			if tt.willError {
+				assert.EqualError(t, err, tt.error.Error())
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
 
 func TestNote_IsExpired(t *testing.T) {
 	tests := []struct {
