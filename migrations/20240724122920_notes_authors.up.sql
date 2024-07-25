e9a64ef00ccbc0aedff3300dc3b5e1edd67f62cc
Smirnov Oleksandr <ss2316544@gmail.com>
2024-07-25 17:10:18 +0300

    fix(notes): notes might be readed and removed after

--- migrations/20240724122920_notes_authors.up.sql
+++ migrations/20240724122920_notes_authors.up.sql
@@ -1,5 +1,5 @@
 create table notes_authors (
   id uuid primary key default uuid_generate_v4(),
-  note_id uuid references notes (id),
-  user_id uuid references users (id)
+  note_id uuid references notes (id) on delete cascade,
+  user_id uuid references users (id) on delete cascade
 );
