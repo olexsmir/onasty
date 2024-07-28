57ad046edbd589d578d14a400f575a9bac4ebeba
Smirnov Oleksandr <ss2316544@gmail.com>
2024-07-28 15:35:56 +0300

    fix migration

--- migrations/20240724122920_notes_authors.up.sql
+++ migrations/20240724122920_notes_authors.up.sql
@@ -1,5 +1,6 @@
 create table notes_authors (
   id uuid primary key default uuid_generate_v4(),
   note_id uuid references notes (id) on delete cascade,
-  user_id uuid references users (id) on delete cascade
+  user_id uuid references users (id) on delete cascade,
+  created_at timestamptz not null default now()
 );
