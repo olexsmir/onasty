50e39fcc87b4c086dc69a740a0315e73733bd599
Smirnov Oleksandr <ss2316544@gmail.com>
2024-07-29 14:49:05 +0300

    feat(mailer): make the simplest impl of email sender

---
+++ internal/mailer/mailgun.go
@@ -0,0 +1,31 @@
+package mailer
+
+import (
+	"context"
+
+	"github.com/mailgun/mailgun-go/v4"
+)
+
+var _ Mailer = (*Mailgun)(nil)
+
+type Mailgun struct {
+	from string
+
+	mg *mailgun.MailgunImpl
+}
+
+func NewMailgun(from, domain, apiKey string) *Mailgun {
+	mg := mailgun.NewMailgun(domain, apiKey)
+	return &Mailgun{
+		from: from,
+		mg:   mg,
+	}
+}
+
+func (m *Mailgun) Send(ctx context.Context, to, subject, content string) error {
+	msg := m.mg.NewMessage(m.from, subject, "", to)
+	msg.SetHtml(content)
+
+	_, _, err := m.mg.Send(ctx, msg)
+	return err
+}
