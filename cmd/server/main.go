58d06cbe7239ac840e36543d4e5c08a72d789f6f
Smirnov Oleksandr <ss2316544@gmail.com>
2024-09-12 13:20:26 +0300

    fix typo

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,4 @@
-//nolint:err113 // all errors are shown to the use so it's ok for them to be dynamic
+//nolint:err113 // all errors are shown to the user so it's ok for them to be dynamic
 package main
 
 import (
