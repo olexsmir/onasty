61c51b2c5849c2bab83ae33850203ad2f8ec8068
Smirnov Oleksandr <ss2316544@gmail.com>
2024-09-15 09:00:29 +0300

    fixup! feat(url): use production url in mails

--- internal/service/usersrv/usersrv.go
+++ internal/service/usersrv/usersrv.go
@@ -96,7 +96,13 @@ func (u *UserSrv) SignUp(ctx context.Context, inp dtos.CreateUserDTO) (uuid.UUID
 	// TODO: handle the error that might be returned
 	// i dont think that tehre's need to handle the error, just log it
 	bgCtx, bgCancel := context.WithTimeout(context.Background(), 10*time.Second)
-	go u.sendVerificationEmail(bgCtx, bgCancel, inp.Email, vtok) //nolint:errcheck,contextcheck
+	go u.sendVerificationEmail( //nolint:errcheck,contextcheck
+		bgCtx,
+		bgCancel,
+		inp.Email,
+		vtok,
+		u.appURL,
+	)
 
 	return uid, nil
 }
@@ -214,7 +220,13 @@ func (u *UserSrv) ResendVerificationEmail(ctx context.Context, inp dtos.SignInDT
 	}
 
 	bgCtx, bgCancel := context.WithTimeout(context.Background(), 10*time.Second)
-	go u.sendVerificationEmail(bgCtx, bgCancel, inp.Email, token) //nolint:errcheck,contextcheck
+	go u.sendVerificationEmail( //nolint:errcheck,contextcheck
+		bgCtx,
+		bgCancel,
+		inp.Email,
+		token,
+		u.appURL,
+	)
 
 	return nil
 }
