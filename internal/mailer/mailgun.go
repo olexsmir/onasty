07f9c924ca86ad57e6df4e4ace335b0195481c40
Smirnov Oleksandr <ss2316544@gmail.com>
2024-10-04 11:46:14 +0300

    feat(mail): add migrations

--- internal/mailer/mailgun.go
+++ internal/mailer/mailgun.go
@@ -5,6 +5,8 @@ import (
 	"log/slog"
 
 	"github.com/mailgun/mailgun-go/v4"
+	"github.com/olexsmir/onasty/internal/metrics"
+	"github.com/olexsmir/onasty/internal/transport/http/reqid"
 )
 
 var _ Mailer = (*Mailgun)(nil)
@@ -27,10 +29,16 @@ func (m *Mailgun) Send(ctx context.Context, to, subject, content string) error {
 	msg := m.mg.NewMessage(m.from, subject, "", to)
 	msg.SetHtml(content)
 
-	_, _, err := m.mg.Send(ctx, msg)
-
 	slog.InfoContext(ctx, "email sent", "to", to)
-	slog.DebugContext(ctx, "email sent", "subject", subject, "content", content, "err", err)
 
-	return err
+	_, _, err := m.mg.Send(ctx, msg)
+	if err != nil {
+		metrics.RecordEmailFailed(reqid.GetContext(ctx))
+		return err
+	}
+
+	slog.DebugContext(ctx, "email sent", "subject", subject, "content", content, "err", err)
+	metrics.RecordEmailSent()
+
+	return nil
 }
