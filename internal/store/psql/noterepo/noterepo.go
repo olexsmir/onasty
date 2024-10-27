390467bb305e9654c692a8c5835a74e11f5859c0
Smirnov Oleksandr <ss2316544@gmail.com>
2024-10-27 13:50:23 +0200

    feat(noterepo): write password

--- internal/store/psql/noterepo/noterepo.go
+++ internal/store/psql/noterepo/noterepo.go
@@ -33,8 +33,8 @@ func New(db *psqlutil.DB) *NoteRepo {
 func (s *NoteRepo) Create(ctx context.Context, inp dtos.CreateNoteDTO) error {
 	query, args, err := pgq.
 		Insert("notes").
-		Columns("content", "slug", "burn_before_expiration ", "created_at", "expires_at").
-		Values(inp.Content, inp.Slug, inp.BurnBeforeExpiration, inp.CreatedAt, inp.ExpiresAt).
+		Columns("content", "slug", "password", "burn_before_expiration ", "created_at", "expires_at").
+		Values(inp.Content, inp.Slug, inp.Password, inp.BurnBeforeExpiration, inp.CreatedAt, inp.ExpiresAt).
 		SQL()
 	if err != nil {
 		return err
