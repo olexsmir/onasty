0691ccd4f0f47dd411aabe3b802ef951a68ac357
Smirnov Oleksandr <ss2316544@gmail.com>
2025-01-01 17:32:04 +0200

    refactor(mailer): send emails in background

--- mailer/service.go
+++ mailer/service.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log/slog"
 )
 
 type Service struct {
@@ -16,25 +17,30 @@ func NewService(appURL string, mg *Mailgun) *Service {
 	}
 }
 
-type sendInput struct {
-	Receiver     string
-	TemplateName string
-	Options      map[string]string
-}
-
-func (s Service) Send(ctx context.Context, inp sendInput) error {
-	tmpl, err := getTemplate(s.appURL, inp.TemplateName)
+func (s Service) Send(
+	ctx context.Context,
+	cancel context.CancelFunc,
+	receiver, templateName string,
+	templateOpts map[string]string,
+) error {
+	tmpl, err := getTemplate(s.appURL, templateName)
 	if err != nil {
 		return err
 	}
 
-	t := tmpl(inp.Options)
+	t := tmpl(templateOpts)
 
-	// prob better to create sep context with time out and use it here
-
-	if err := s.mg.Send(ctx, inp.Receiver, t.Subject, t.Body); err != nil {
-		return err
-	}
+	go func() {
+		select {
+		case <-ctx.Done():
+			slog.ErrorContext(ctx, "failed to send verification email", "err", ctx.Err())
+		default:
+			if err := s.mg.Send(ctx, receiver, t.Subject, t.Body); err != nil {
+				slog.ErrorContext(ctx, "failed to send verification email", "err", err)
+			}
+			cancel()
+		}
+	}()
 
 	return nil
 }
