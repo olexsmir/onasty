bfdb6afb04d252f46a9c502d7821f5f76c273625
Oleksandr Smirnov <olexsmir@gmail.com>
2025-01-06 19:33:54 +0200

    refactor(mailermq): use context because it can use context

--- internal/events/mailermq/mailermq.go
+++ internal/events/mailermq/mailermq.go
@@ -3,9 +3,9 @@ package mailermq
 import (
 	"context"
 	"encoding/json"
-	"time"
 
 	"github.com/nats-io/nats.go"
+	"github.com/olexsmir/onasty/internal/events"
 	"github.com/olexsmir/onasty/internal/transport/http/reqid"
 )
 
@@ -13,10 +13,7 @@ type MailerMQ struct {
 	nc *nats.Conn
 }
 
-const (
-	sendMailSubject = "mailer.send"
-	sendMailTimeout = 5 * time.Second
-)
+const sendMailSubject = "mailer.send"
 
 func New(nc *nats.Conn) *MailerMQ {
 	return &MailerMQ{
@@ -24,7 +21,7 @@ func New(nc *nats.Conn) *MailerMQ {
 	}
 }
 
-type SendRequest struct {
+type sendRequest struct {
 	RequestID    string            `json:"request_id"`
 	Receiver     string            `json:"receiver"`
 	TemplateName string            `json:"template_name"`
@@ -40,7 +37,7 @@ func (m MailerMQ) SendVerificationEmail(
 	ctx context.Context,
 	inp SendVerificationEmailRequest,
 ) error {
-	req, err := json.Marshal(SendRequest{
+	req, err := json.Marshal(sendRequest{
 		RequestID:    reqid.GetContext(ctx),
 		Receiver:     inp.Receiver,
 		TemplateName: "email_verification",
@@ -52,12 +49,10 @@ func (m MailerMQ) SendVerificationEmail(
 		return err
 	}
 
-	_, err = m.nc.Request(sendMailSubject, req, sendMailTimeout)
+	resp, err := m.nc.RequestWithContext(ctx, sendMailSubject, req)
 	if err != nil {
 		return err
 	}
 
-	// TODO: handle error that service might return
-
-	return err
+	return events.CheckRespForError(resp)
 }
