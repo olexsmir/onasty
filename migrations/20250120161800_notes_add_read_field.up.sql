ea18a37174b533f8cd9f29939c80f772885c4fbf
Oleksandr Smirnov <olexsmir@gmail.com>
2025-01-30 16:07:42 +0200

    fixup! chore(migrate): add fields to indicate if note was read

--- migrations/20250120161800_notes_add_read_field.up.sql
+++ migrations/20250120161800_notes_add_read_field.up.sql
@@ -1,3 +1,3 @@
 ALTER TABLE notes
     ADD COLUMN "is_read" boolean DEFAULT FALSE NOT NULL,
-    ADD COLUMN "read_at" timestamp;
+    ADD COLUMN "read_at" timestamptz DEFAULT '1970-01-01 00:00:00+00'::timestamptz NOT NULL;
