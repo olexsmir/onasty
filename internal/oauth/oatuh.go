92e2e48e90ac4e6f12231dea2bdba0ad0ae0d104
Oleksandr Smirnov <olexsmir@gmail.com>
2025-05-03 21:00:01 +0300

    docs(oauth): add comments

--- internal/oauth/oatuh.go
+++ internal/oauth/oatuh.go
@@ -2,17 +2,23 @@ package oauth
 
 import "context"
 
+// Provider is an OAuth interface.
 type Provider interface {
-	// GetAuthURL generates the URL to redirect users to for login.
+	// GetAuthURL return the provider's authorization page URL.
 	GetAuthURL(state string) string
 
-	// ExchangeCode exchanges the authorization code for an access token and ID token.
+	// ExchangeCode exchanges the provided authorization code for user information.
 	ExchangeCode(ctx context.Context, code string) (UserInfo, error)
 }
 
+// UserInfo represents the user information returned by the OAuth provider.
 type UserInfo struct {
-	Provider      string
-	ProviderID    string
-	Email         string
+	// Provider is the name of the OAuth provider
+	Provider string
+	// ProviderID is user ID assigned by the provider
+	ProviderID string
+	// Email is user's email address returned by the provider
+	Email string
+	// EmailVerified indicates whether the email was verified by the provider
 	EmailVerified bool
 }
