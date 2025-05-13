90a10d90abc28d49f9ff6ac432e2eec7b04192c8
Oleksandr Smirnov <olexsmir@gmail.com>
2025-05-13 21:04:05 +0300

    fixup! feat: add password reset tokens repo

--- migrations/20250509131258_password_reset_tokens.up.sql
+++ migrations/20250509131258_password_reset_tokens.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE password_reset_tokens (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
-    user_id uuid NOT NULL UNIQUE REFERENCES users (id),
+    user_id uuid NOT NULL REFERENCES users (id),
     token varchar(255) NOT NULL UNIQUE,
     created_at timestamptz NOT NULL DEFAULT now(),
     expires_at timestamptz NOT NULL,
