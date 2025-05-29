8355b11c4a5ab3f92afdc08deca862e5d3785c60
Oleksandr Smirnov <olexsmir@gmail.com>
2025-05-30 00:35:27 +0300

    feat(seed): add notes seeder

--- cmd/seed/notes.go
+++ cmd/seed/notes.go
@@ -2,25 +2,108 @@ package main
 
 import (
 	"context"
-	"errors"
+	"log/slog"
 	"time"
 
-	"github.com/olexsmir/onasty/internal/models"
-	"github.com/olexsmir/onasty/internal/store/psql/noterepo"
+	"github.com/olexsmir/onasty/internal/hasher"
+	"github.com/olexsmir/onasty/internal/store/psqlutil"
 )
 
-func seedNotes(ctx context.Context, repo noterepo.NoteStorer) error {
-	var errs error
-	err := repo.Create(ctx, models.Note{ //nolint:exhaustruct
-		Content:              "that is the test note",
-		Slug:                 "test1",
-		Password:             "",
-		BurnBeforeExpiration: false,
-		CreatedAt:            time.Now(),
-	})
-	if err != nil {
-		errs = errors.Join(errs, err)
+var notesData = []struct {
+	id                   string
+	content              string
+	slug                 string
+	burnBeforeExpiration bool
+	password             string
+	expiresAt            time.Time
+	hasAuthor            bool
+	authorID             int
+}{
+	{
+		content:              "that test note one",
+		slug:                 "one",
+		burnBeforeExpiration: false,
+	},
+	{
+		content:              "that test note two",
+		slug:                 "two",
+		burnBeforeExpiration: true,
+		password:             "",
+		expiresAt:            time.Now().Add(24 * time.Hour),
+	},
+	{
+		content:              "that passworded note",
+		slug:                 "passwd",
+		burnBeforeExpiration: false,
+		password:             "pass",
+	},
+	{
+		content:              "that note with author",
+		slug:                 "user",
+		burnBeforeExpiration: false,
+		hasAuthor:            true,
+		authorID:             0,
+	},
+}
+
+func seedNotes(
+	ctx context.Context,
+	hash hasher.Hasher,
+	db *psqlutil.DB,
+) error {
+	for i, note := range notesData {
+		passwd := ""
+		if note.password != "" {
+			var err error
+			passwd, err = hash.Hash(note.password)
+			if err != nil {
+				return err
+			}
+		}
+
+		slog.Info("note", "slug", note.slug, "author", note.authorID)
+
+		err := db.QueryRow(
+			ctx, `
+		insert into notes (content, slug, burn_before_expiration, password, expires_at)
+		values ($1, $2, $3, $4, $5)
+		on conflict (slug) do update set
+			content = excluded.content,
+			slug = excluded.slug,
+			burn_before_expiration = excluded.burn_before_expiration,
+			password = excluded.password,
+			expires_at = excluded.expires_at
+		returning id`,
+			note.content,
+			note.slug,
+			note.burnBeforeExpiration,
+			passwd,
+			note.expiresAt,
+		).Scan(&notesData[i].id)
+		if err != nil {
+			return err
+		}
+
+		if note.hasAuthor {
+			slog.Info("setting author", "note", note.id, "author", note.authorID)
+			if err := setAuthor(ctx, db, notesData[i].id, usersData[note.authorID].id); err != nil {
+				return err
+			}
+		}
 	}
 
-	return errs
+	return nil
+}
+
+func setAuthor(
+	ctx context.Context,
+	db *psqlutil.DB,
+	noteID string,
+	authorID string,
+) error {
+	_, err := db.Exec(
+		ctx,
+		`insert into notes_authors (note_id, user_id) values ($1, $2)`,
+		noteID, authorID)
+	return err
 }
