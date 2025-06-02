f5ea1d8c2278596748f50df31d2b515edb4a39bb
Oleksandr Smirnov <olexsmir@gmail.com>
2025-06-02 16:01:06 +0300

    test(e2e): delete note

--- e2e/apiv1_notes_authorized_test.go
+++ e2e/apiv1_notes_authorized_test.go
@@ -22,3 +22,34 @@ func (e *AppTestSuite) TestNoteV1_Create_authorized() {
 	e.Equal(http.StatusCreated, httpResp.Code)
 	e.Equal(dbNote.ID.String(), dbNoteAuthor.noteID.String())
 }
+
+func (e *AppTestSuite) TestNoteV1_Delete() {
+	_, toks := e.createAndSingIn(e.uuid()+"@test.com", "password")
+	httpResp := e.httpRequest(
+		http.MethodPost,
+		"/api/v1/note",
+		e.jsonify(apiv1NoteCreateRequest{ //nolint:exhaustruct
+			Content: "some random ass content for the test",
+		}),
+		toks.AccessToken,
+	)
+
+	e.Equal(httpResp.Code, http.StatusCreated)
+
+	var body apiv1NoteCreateResponse
+	e.readBodyAndUnjsonify(httpResp.Body, &body)
+
+	dbNote := e.getNoteBySlug(body.Slug)
+	e.NotEmpty(dbNote)
+
+	httpResp = e.httpRequest(
+		http.MethodDelete,
+		"/api/v1/note/"+body.Slug+"/delete",
+		nil,
+		toks.AccessToken,
+	)
+	e.Equal(httpResp.Code, http.StatusNoContent)
+
+	dbNote = e.getNoteBySlug(body.Slug)
+	e.Empty(dbNote)
+}
