fc18a43c7a8bfff123290d28c6fd049a996e8cfe
Oleksandr Smirnov <olexsmir@gmail.com>
2025-06-18 15:34:32 +0300

    chore: boot strap elm

---
+++ web/tests/UnitTests/Data/Credentiala.elm
@@ -0,0 +1,11 @@
+module UnitTests.Data.Credentiala exposing (suite)
+
+import Expect
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    test "for future" <|
+        \_ ->
+            Expect.equal True True
