0271ef8d88e4df73d09cf619ecf0f90daf6d29ff
Oleksandr Smirnov <olexsmir@gmail.com>
2025-06-20 02:08:23 +0300

    web: fix formatting of interop.js

--- web/src/interop.js
+++ web/src/interop.js
@@ -1,16 +1,16 @@
 import "./styles.css";
 
 export const flags = (_) => {
-    return {
-        access_token: JSON.parse(window.localStorage.access_token || 'null'),
-        refresh_token: JSON.parse(window.localStorage.refresh_token || 'null'),
-    }
+  return {
+    access_token: JSON.parse(window.localStorage.access_token || "null"),
+    refresh_token: JSON.parse(window.localStorage.refresh_token || "null"),
+  }
 }
 
 export const onReady = ({ app }) => {
-    if (app.ports?.sendToLocalStorage) {
-        app.ports.sendToLocalStorage.subscribe(({ key, value }) => {
-            window.localStorage[key] = JSON.stringify(value);
-        })
-    }
+  if (app.ports?.sendToLocalStorage) {
+    app.ports.sendToLocalStorage.subscribe(({ key, value }) => {
+      window.localStorage[key] = JSON.stringify(value);
+    })
+  }
 }
