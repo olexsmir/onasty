0d7940f0c53fbb7f7f6c42ed687c981e95a7fa9a
Oleksandr Smirnov <olexsmir@gmail.com>
2025-06-20 14:31:41 +0300

    fixup! web: fix formatting of interop.js

--- web/src/interop.js
+++ web/src/interop.js
@@ -4,13 +4,13 @@ export const flags = (_) => {
   return {
     access_token: JSON.parse(window.localStorage.access_token || "null"),
     refresh_token: JSON.parse(window.localStorage.refresh_token || "null"),
-  }
-}
+  };
+};
 
 export const onReady = ({ app }) => {
   if (app.ports?.sendToLocalStorage) {
     app.ports.sendToLocalStorage.subscribe(({ key, value }) => {
       window.localStorage[key] = JSON.stringify(value);
-    })
+    });
   }
-}
+};
