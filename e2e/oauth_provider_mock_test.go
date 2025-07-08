242bfccd6b126fb14aadacb3fdd4f9b2e8f2977e
Olexandr Smirnov <olexsmir@gmail.com>
2025-07-08 17:16:51 +0300

    refactor(e2e): name stubs as stubs

--- e2e/oauth_provider_mock_test.go
+++ e2e/oauth_provider_mock_test.go
@@ -6,19 +6,19 @@ import (
 	"github.com/olexsmir/onasty/internal/oauth"
 )
 
-var _ oauth.Provider = (*oauthProviderMock)(nil)
+var _ oauth.Provider = (*oauthProviderStub)(nil)
 
-type oauthProviderMock struct{}
+type oauthProviderStub struct{}
 
-func newOauthProviderMock() *oauthProviderMock {
-	return &oauthProviderMock{}
+func newOauthProviderStub() *oauthProviderStub {
+	return &oauthProviderStub{}
 }
 
-func (o *oauthProviderMock) GetAuthURL(_ string) string {
+func (o *oauthProviderStub) GetAuthURL(_ string) string {
 	return "https://example.com/oauth/authorize"
 }
 
-func (o *oauthProviderMock) ExchangeCode(_ context.Context, _ string) (oauth.UserInfo, error) {
+func (o *oauthProviderStub) ExchangeCode(_ context.Context, _ string) (oauth.UserInfo, error) {
 	return oauth.UserInfo{
 		Provider:      "google",
 		ProviderID:    "1234567890",
