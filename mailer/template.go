4b0af5b02ac6716af8983a7f1048039e8222973d
Olexandr Smirnov <olexsmir@gmail.com>
2025-08-21 20:51:33 +0300

    fix(mailer): fix templates

--- mailer/template.go
+++ mailer/template.go
@@ -29,26 +29,30 @@ func getTemplate(appURL, frontendURL string, templateName string) (TemplateFunc,
 
 func emailVerificationTemplate(appURL string) TemplateFunc {
 	return func(opts map[string]string) Template {
+		link := fmt.Sprintf("%[1]s/api/v1/auth/verify/%[2]s", appURL, opts["token"])
+
 		return Template{
 			Subject: "Onasty: verify your email",
 			Body: fmt.Sprintf(`To verify your email, please follow this link:
-<a href="%[1]s/api/v1/auth/verify/%[2]s">%[1]s/api/v1/auth/verify/%[2]s</a>
-<br />
-<br />
-This link will expire after 24 hours.`, appURL, opts["token"]),
+<a href="%[1]s">%[1]s</a>
+<br>
+<br>
+This link will expire after 24 hours.`, link),
 		}
 	}
 }
 
 func passwordResetTemplate(frontendURL string) TemplateFunc {
 	return func(opts map[string]string) Template {
+		link := fmt.Sprintf("%[1]s/auth?token=%[2]s", frontendURL, opts["token"])
+
 		return Template{
 			Subject: "Onasty: reset your password",
 			Body: fmt.Sprintf(`To reset your password, use this api:
-<a href="%[1]s/auth?token=%[2]s">%[1]s/auth?token=%[2]s</a>
-<br />
-<br />
-This link will expire after an hour.`, frontendURL, opts["token"]),
+<a href="%[1]s">%[1]s</a>
+<br>
+<br>
+This link will expire after an hour.`, link),
 		}
 	}
 }
