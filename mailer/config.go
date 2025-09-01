cf0d1e4336dc6481e303394564727612c58dc2ff
Oleksandr Smirnov <olexsmir@gmail.com>
2025-09-01 12:47:45 +0300

    refactor(config): make config singleton

--- mailer/config.go
+++ mailer/config.go
@@ -3,6 +3,12 @@ package main
 import (
 	"os"
 	"strconv"
+	"sync"
+)
+
+var (
+	configInstance *Config
+	once           sync.Once
 )
 
 type Config struct {
@@ -23,19 +29,22 @@ type Config struct {
 }
 
 func NewConfig() *Config {
-	return &Config{
-		AppURL:         getenvOrDefault("APP_URL", ""),
-		FrontendURL:    getenvOrDefault("FRONTEND_URL", ""),
-		NatsURL:        getenvOrDefault("NATS_URL", ""),
-		MailgunFrom:    getenvOrDefault("MAILGUN_FROM", ""),
-		MailgunDomain:  getenvOrDefault("MAILGUN_DOMAIN", ""),
-		MailgunAPIKey:  getenvOrDefault("MAILGUN_API_KEY", ""),
-		LogLevel:       getenvOrDefault("LOG_LEVEL", "debug"),
-		LogFormat:      getenvOrDefault("LOG_FORMAT", "json"),
-		LogShowLine:    getenvOrDefault("LOG_SHOW_LINE", "true") == "true",
-		MetricsPort:    mustGetenvOrDefaultInt("METRICS_PORT", 8001),
-		MetricsEnabled: getenvOrDefault("METRICS_ENABLED", "true") == "true",
-	}
+	once.Do(func() {
+		configInstance = &Config{
+			AppURL:         getenvOrDefault("APP_URL", ""),
+			FrontendURL:    getenvOrDefault("FRONTEND_URL", ""),
+			NatsURL:        getenvOrDefault("NATS_URL", ""),
+			MailgunFrom:    getenvOrDefault("MAILGUN_FROM", ""),
+			MailgunDomain:  getenvOrDefault("MAILGUN_DOMAIN", ""),
+			MailgunAPIKey:  getenvOrDefault("MAILGUN_API_KEY", ""),
+			LogLevel:       getenvOrDefault("LOG_LEVEL", "debug"),
+			LogFormat:      getenvOrDefault("LOG_FORMAT", "json"),
+			LogShowLine:    getenvOrDefault("LOG_SHOW_LINE", "true") == "true",
+			MetricsPort:    mustGetenvOrDefaultInt("METRICS_PORT", 8001),
+			MetricsEnabled: getenvOrDefault("METRICS_ENABLED", "true") == "true",
+		}
+	})
+	return configInstance
 }
 
 func getenvOrDefault(key, def string) string {
